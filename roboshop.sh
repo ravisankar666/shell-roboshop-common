c1ce7dd4fb6c793a131a5cbb580607750b1024b0
ravisankar666 <ravisankarp666@gmail.com>
2025-09-28 22:58:32 +0530

    some message

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ SG_ID="sg-00c2d6548a23412ff" # replace security group id
 ZONE_ID="Z0043313JVXOCUVVVC7F" # replace with hosted_zone id
 for instance in $@
 do  
-  INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output tex)
+  INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
   #get private ID
   if [ $instance != "frontend" ]; then 
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
