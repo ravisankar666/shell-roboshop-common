699d861350e8567ca31c79e6ee29ba4fed9fdbd6
ravisankar666 <ravisankarp666@gmail.com>
2025-09-30 02:11:33 +0530

    some message

--- catalogue.sh
+++ catalogue.sh
@@ -14,12 +14,12 @@ VALIDATE $? "copy mongo repo"
 dnf install mongodb-mongosh -y &>>LOG_FILE
 VALIDATE $? "installing mongodb client"
 
-INDEX=$(mongosh mongodb.daws86.fun --quiet --eval "db.getmongo().getDBName().indexOf('catalogue')")
-if [ $INDEX -le 0 ]; then 
-   mongosh --host $MONOGDB_HOST </app/db/master-data.js &>>$LOG_FILE
-   VALIDATE $? "Load $app_name product"
-else 
-   echo -e "$app_name product already loaded ... $Y SKIPPING $N"
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load $app_name products"
+else
+    echo -e "$app_name products already loaded ... $Y SKIPPING $N"
 fi
 
 app_restart
