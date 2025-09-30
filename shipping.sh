42ff281b93773c151269bcd87df9c547a6bd3e25
ravisankar666 <ravisankarp666@gmail.com>
2025-10-01 00:57:42 +0530

    some message

--- shipping.sh
+++ shipping.sh
@@ -13,8 +13,11 @@ dnf install mysql -y  &>>$LOG_FILE
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+    VALIDATE $? "schema loading "
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
+    VALIDATE $? "user loading"
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+    VALIDATE $? "master-data loading"
 else
     echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
 fi
